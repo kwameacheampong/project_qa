b4a2755dbfcaa54993dbd5e406565c01eb589519
Ubuntu <ignat@development-server-jenkins.z0wdas3oujmethwmnw2lndpdda.fx.internal.cloudapp.net>
2021-12-02 09:20:04 +0000

    adding changes

--- frontend/application/forms.py
+++ frontend/application/forms.py
@@ -3,12 +3,21 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class CreateAwardForm(FlaskForm):
-    description = 
-    name = StringField("Award Name", validators=[DataRequired()])
-    player_name = SelectField("Player_Name",
-     choices=[
-         ('Messi', 'Messi'),
-         ('Ronaldo', 'Ronaldo'),
-         ('Salah', 'Salah'),
-     ])
-    submit = SubmitField("Add Award to a Player")
+    award = StringField("Award Name", validators=[DataRequired()])
+    name = StringField("Award Player", validators=[DataRequired()]),
+    club = SelectField("Award Club", validators=[DataRequired()]),
+    stats = StringField("Award Stats", validators=[DataRequired()],
+    
+    choices=[
+        ('Best player'), ('Best player')
+        ('Topscorer', 'Topscorer'),
+        ('Most asist', 'Most asist'),
+    ])
+    submit = SubmitField("Add Award ")
+
+class CreatePlayerForm(FlaskForm):
+    name = StringField("Player Name", validators=[DataRequired()]),
+    club = SelectField("Player Club", validators=[DataRequired()]),
+    stats = StringField("Player Stats", validators=[DataRequired()], choices[])
+    
+    submit = SubmitField("Add Player")
--- frontend/application/routes.py
+++ frontend/application/routes.py
@@ -1,107 +1,93 @@
 from application import app, db
-from application.models import Awards
-from application.forms import AwardsForm
-from flask import render_template, request, redirect, url_for, jsonify
-from os import getenv
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
 
-@app.route('/')
-@app.route('/home')
+backend = getenv("BACKEND_HOSTNAME")
+
+@app.route('/', methods=["GET"])
 def home():
-    all_awards = awards.query.all()
-    return render_template('index.html', title="Home", all_awards=all_awards)
+    awards = request.get(f"http://{backend}/get/allAwards").json()["awards"]
+    return render_template('index.html', title="Home", Awards=Awards)
 
-@app.route('/create/player/<int:award_id>', methods=['GET','POST'])
-def create_player(award_id):
-    json = request.json
-    new_player = Player(
-        name = json["name"],
-        award_id = award_id,
-        stats = json["stats"]
-    )
-        db.session.add(new_award)
-        db.session.commit()
-    return render_template("create_award.html", title="Add a new Award", form=form)
+@app.route('/create/award', methods=['GET','POST'])
+def create_award():
+    form = CreateAwardForm()
 
-@app.route('/get/allawards', method=["GET"])
-def get_all_awards():
-    all_awards = Awards.query.all()
-    json = {"awards": []}
-    for award in all_awards:
-        player =[]
-        for player in award.players:
-            award.append(        
-            {
-                "id": award.id,
-                "name": player.
+    if request.method == "POST":
+        response = request.post(
+            f"http://{backend_host}/create/award", 
+            json={
+                "name": form.name.data,
+                "club": form.name.data,
+                "stats": form.name.data
+            }
+        )
+        app.logger.info(f"Response: {response.txt")
+        return redirect(url_for('home'))
+       
+    return render_template("create_awards.html", title="Add a new Awards", form=form)
+
+@app.route('/create/player', methods=['GET','POST'])
+def create_player():
+    form = CreatePlayerForm()
+
+    if request.method == "POST":
+        response = request.post(
+            f"http://{backend_host}/create/player/{award_id}",
+            json={
+                "name": form.name.data,
+                "club": form.name.data,
+            }
+        )
+        app.logger.info(f"Response: {response.txt")
+        return redirect(url_for('home'))
+       
+    return render_template("create_player.html", title="Add Players", form=form)
+
+
+@app.route('/read/allPlayer')
+def read_player():
+     
+    Awards = award.query.all()
+    awards_dict = {"awards": []}
+    for awards in Awards:
+        awards_dict["awards"].append(   
+                      {
+                "description": awards.description,
+                "completed": awards.completed
             }
         )
     return jsonify(awards_dict)
 
-@app.route('/update/award/<int:id>', methods=['GET','POST'])
-def update_award(id):
-    form = AwardForm()
-    award = Awards.query.get(id)
+@app.route('/update/awards/<int:id>', methods=['GET','POST'])
+def update_Awards(id):
+    form = AwardsForm()
+    awards = Awards.query.get(id)
 
     if request.method == "POST":
-        award.description = form.description.data
+        awards.description = form.description.data
         db.session.commit()
         return redirect(url_for('home'))
 
-    return render_template('update_award.html', award=award, form=form)
+    return render_template('update_awards.html', awards=awards, form=form)
 
-@app.route('/delete/award/<int:id>')
-def delete_Award(id):
-    award = Awards.query.get(id)
-    db.session.delete(award)
+@app.route('/delete/awards/<int:id>')
+def delete_Awards(id):
+    awards = Awards.query.get(id)
+    db.session.delete(awards)
     db.session.commit()
     return redirect(url_for('home'))
 
-@app.route('/complete/award/<int:id>')
-def complete_Award(id):
-    award = awards.query.get(id)
-    award.completed = True
+@app.route('/complete/awards/<int:id>')
+def complete_Awards(id):
+    awards = Awards.query.get(id)
+    awards.completed = True
     db.session.commit()
     return redirect(url_for('home'))
 
-@app.route('/incomplete/award/<int:id>')
-def incomplete_awards(id):
+@app.route('/incomplete/awards/<int:id>')
+def incomplete_Awards(id):
     awards = Awards.query.get(id)
     awards.completed = False
     db.session.commit()
-    return redirect(url_for('home'))
-
-@app.route('/create/award', methods=['GET','POST'])
-def create_awards():
-    form = AwardsForm()
-
-    if request.method == "POST":
-        new_awards = Awardss(description=form.description.data)
-        db.session.add(new_awards)
-        db.session.commit()
-        return redirect(url_for('home'))
-
-    return render_template("create_award.html", title="Add a new Award", form=form)
-
- @app.route('/create/award', methods=['GET','POST'])
-def create_award():
-    form = awardsForm()
-
-    if request.method == "POST":
-        new_awards = awardss(description=form.description.data)
-        db.session.add(new_award)
-        db.session.commit()
-        return redirect(url_for('home'))
-
-    return render_template("create_award.html", title="Add a new Award", form=form)
-
-@app.route('/create/award', methods=['GET','POST'])
-def create_award():
-    form = awardsForm()
-
-    if request.method == "POST":
-        new_award = Award(description=form.description.data)
-        db.session.add(new_award)
-        db.session.commit()
-        return redirect(url_for('home'))
-
-    return render_template("create_award.html", title="Add a new Award", form=form)           
+    return redirect(url_for('home')) 
